8b8e435eb1cbc9322e65ac9fa11ac295bf8ac0de
ncou <nicolas_courade@yahoo.fr>
2022-05-18 15:50:01 +0200

    fix: use correct class name for the extends in the TestCase classe

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace Vendor\Test\SubNamespace;
 
-use Chiron\Dev\Tools\TestCase as BaseTestCase;
+use Chiron\Dev\Tools\TestSuite\AbstractTestCase;
 
 /**
  * A base test case for common test functionality
  */
-class TestCase extends BaseTestCase
+class TestCase extends AbstractTestCase
 {
 }
